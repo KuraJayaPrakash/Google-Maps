871267173b1e07555c6357a04174f85d6e7e1259
KuraJayaPrakash <81488572+KuraJayaPrakash@users.noreply.github.com>
2021-03-28 08:58:19 +0530

    Time taken to travel between 2 places in order to send an E-mail

--- fare-to-travel-between-two-places.py
+++ fare-to-travel-between-two-places.py
@@ -1,27 +1,60 @@
 import requests
-import json
+import smtplib 
 
-key="AIzaSyDHkUK9MjmEWAQQK5Vyc4dvMkRXbB0t3eU"  ##if there is an error while executing this program "index out of range" just change this key value by generating a new key form the site: https://console.cloud.google.com/apis/credentials?project=myproject-244611 ---> create credentials ---> API key
-source="Churchgate station"
-destination="Borivali station"
-url="https://maps.googleapis.com/maps/api/distancematrix/json?origins="+source+"&destinations="+destination+"&key="+key
+# API key
+api_file = open("api-key.txt", "r")
+api_key = api_file.readline()
+api_file.close()
 
-print(url)
+# home address input
+home = input("Enter a home address\n") 
+  
+# work address input
+work = input("Enter a work address\n") 
+  
+# base url
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
 
-a=requests.get(url)
+# get response
+r = requests.get(url + "origins=" + home + "&destinations=" + work + "&key=" + api_key) 
+ 
+# return time as text and as seconds
+time = r.json()["rows"][0]["elements"][0]["duration"]["text"]       
+seconds = r.json()["rows"][0]["elements"][0]["duration"]["value"]
+  
+# print the travel time
+print("\nThe total travel time from home to work is", time)
 
-print(a, a.text, type(a.text)) 
+# check if travel time is more than 1 hour
+if (seconds > 3600):
+    # email constraints
+    sender = "..."    
+    recipient = "..."       
+    subject = "Sick Day"   
+    message = "Hi Team,\n\nSorry, but I can't make it into work today."
+    
+    # format email
+    email = "Subject: {}\n\n{}".format(subject, message)
+    
+    # get sender password
+    password_file = open("password.txt", "r")
+    password = password_file.readline()
+    password_file.close()
+    
+    # creates SMTP session 
+    s = smtplib.SMTP("smtp.gmail.com", 587) 
+      
+    # start TLS for security 
+    s.starttls() 
+      
+    # authentication 
+    s.login(sender, password) 
+      
+    # sending the mail 
+    s.sendmail(sender, recipient, email)
+      
+    # terminating the session 
+    s.quit() 
 
-b = json.loads(a.text)
-a=b['rows'][0]['elements'][0]['distance']['text']
-
-print(a)
-
-x=float(a.split(" km")[0])
-ans_rick=x*18
-ans_cab=x*40
-ans_bus=x*8
-
-print(str(ans_rick)+"Rickshaw")
-print(str(ans_cab)+"Cab")
-print(str(ans_bus)+"Bus")
+    # success message
+    print("\nSuccessfully sent a sick-day email to", recipient, "since the travel time was too long")
